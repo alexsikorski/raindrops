615efeab39c752a1793d16e44c800ea5065ee209
Alex Sikorski <alex@alexsikorski.net>
2021-05-25 20:34:31 +0100

    added 4 basic tests checking for pling, plang, plong and digit returns

--- src/test/java/com/sparta/as/RaindropsTest.java
+++ src/test/java/com/sparta/as/RaindropsTest.java
@@ -1,14 +1,40 @@
 package com.sparta.as;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+public class RaindropsTest {
 
-public class RaindropsTest
-{
+    private static Raindrops raindrops;
+
+    @BeforeAll
+    static void setUpAll() {
+        raindrops = new Raindrops();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @DisplayName("checkInput3ReturnsPling")
+    void checkInput3ReturnsPling() {
+        Assertions.assertEquals("Pling", raindrops.plingPlangPlong(3));
+    }
+
+    @Test
+    @DisplayName("checkInput5ReturnsPlang")
+    void checkInput5ReturnsPlang() {
+        Assertions.assertEquals("Plang", raindrops.plingPlangPlong(5));
+    }
+
+    @Test
+    @DisplayName("checkInput7ReturnsPlong")
+    void checkInput7ReturnsPlong() {
+        Assertions.assertEquals("Plong", raindrops.plingPlangPlong(7));
+    }
+
+    @Test
+    @DisplayName("checkInput34ReturnsItselfAsString")
+    void checkInput34ReturnsItselfAsString() {
+        Assertions.assertEquals("34", raindrops.plingPlangPlong(34));
     }
 }
