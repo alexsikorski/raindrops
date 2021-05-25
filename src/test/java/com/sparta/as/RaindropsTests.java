7b5c5ac51e3358f5fdaae5d9f1914fb58c2b5b93
Alex Sikorski <alex@alexsikorski.net>
2021-05-25 21:08:09 +0100

    renamed test class to plural tests

--- src/test/java/com/sparta/as/RaindropsTests.java
+++ src/test/java/com/sparta/as/RaindropsTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class RaindropsTest {
+public class RaindropsTests {
 
     private static Raindrops raindrops;
 
