2a3e5f499cfca881006d24efc1077f8f4fb3ec4a
Alex Sikorski <alex@alexsikorski.net>
2021-05-25 19:55:13 +0100

    initial commit

---
+++ src/main/java/com/sparta/as/Raindrops.java
@@ -0,0 +1,6 @@
+package com.sparta.as;
+
+public class Raindrops
+{
+
+}
---
+++ src/test/java/com/sparta/as/RaindropsTest.java
@@ -0,0 +1,14 @@
+package com.sparta.as;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RaindropsTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
