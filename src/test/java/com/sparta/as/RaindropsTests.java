4ff3d5d80efe631689b90521dfc6ae73281ec831
Alex Sikorski <alex@alexsikorski.net>
2021-05-26 18:27:51 +0100

    added negative numbers as parameters for all tests as noted in the readme

--- src/test/java/com/sparta/as/RaindropsTests.java
+++ src/test/java/com/sparta/as/RaindropsTests.java
@@ -3,7 +3,6 @@ package com.sparta.as;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -17,57 +16,56 @@ public class RaindropsTests {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {3})
+    @ValueSource(ints = {3, -3})
     @DisplayName("Check that numbers with only a factor of 3 return 'Pling'")
     void checkThatNumbersWithOnlyAFactorOf3ReturnPling(int number) {
         Assertions.assertEquals("Pling", raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {5})
+    @ValueSource(ints = {5, -24})
     @DisplayName("Check that numbers with only a factor of 5 return 'Plang'")
     void checkThatNumbersWithOnlyAFactorOf5ReturnPlang(int number) {
         Assertions.assertEquals("Plang", raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {7})
+    @ValueSource(ints = {7, -14})
     @DisplayName("Check that numbers with only a factor of 7 return 'Plong'")
     void checkThatNumbersWithOnlyAFactorOf7ReturnPlong(int number) {
         Assertions.assertEquals("Plong", raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {34, 0})
+    @ValueSource(ints = {34, 0, -11})
     @DisplayName("Check that numbers with no respective factors return themselves as a string")
     void checkThatNumbersWithNoRespectiveFactorsReturnThemselvesAsAString(int number) {
         Assertions.assertEquals("" + number + "", raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {15})
+    @ValueSource(ints = {15, -30})
     @DisplayName("Check that numbers with a factor of 3 and 5 return 'PlingPlang'")
     void checkThatNumbersWithAFactorOf3And5ReturnPlingPlang(int number) {
         Assertions.assertEquals("PlingPlang", raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {21})
+    @ValueSource(ints = {21, -42})
     @DisplayName("Check that numbers with a factor of 3 and 7 return 'PlingPlong'")
     void checkThatNumbersWithAFactorOf3And7ReturnPlingPlong(int number) {
         Assertions.assertEquals("PlingPlong", raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {35})
+    @ValueSource(ints = {35, -70})
     @DisplayName("Check that numbers with a factor of 5 and 7 return 'PlangPlong'")
     void checkThatNumbersWithAFactorOf5And7ReturnPlangPlong(int number) {
         Assertions.assertEquals("PlangPlong", raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {105})
-    @Test
+    @ValueSource(ints = {105, -210})
     @DisplayName("Check that numbers with a factor of 3, 5 and 7 return 'PlingPlangPlong'")
     void checkThatNumbersWithAFactorOf35And7ReturnPlingPlangPlong(int number) {
         Assertions.assertEquals("PlingPlangPlong", raindrops.plingPlangPlong(number));
