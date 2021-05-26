5e4f9ae92a3c25b8766cbdd45df9f949c1b2b1af
Alex Sikorski <alex@alexsikorski.net>
2021-05-26 19:09:46 +0100

    created isPlingFactor method that returns string Pling

--- src/main/java/com/sparta/as/Raindrops.java
+++ src/main/java/com/sparta/as/Raindrops.java
@@ -1,6 +1,12 @@
 package com.sparta.as;
 
-public class Raindrops
-{
+public class Raindrops {
 
+    public String plingPlangPlong(int number) {
+        return null;
+    }
+
+    private String isPlingFactor(int number) {
+        return number % 3 == 0 ? "Pling" : "";
+    }
 }
