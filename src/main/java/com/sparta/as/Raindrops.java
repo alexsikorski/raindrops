65d5dc010549f9395b90da7ab790c137753a5cd6
Alex Sikorski <alex@alexsikorski.net>
2021-05-26 19:32:14 +0100

    nested test class for isPl(i/a/o)ngFactor methods, implemented private static final for expected results

--- src/main/java/com/sparta/as/Raindrops.java
+++ src/main/java/com/sparta/as/Raindrops.java
@@ -7,15 +7,15 @@ public class Raindrops {
         return output.isEmpty() ? "" + number + "" : output;
     }
 
-    private String isPlingFactor(int number) {
+    public String isPlingFactor(int number) {
         return number != 0 && number % 3 == 0 ? "Pling" : "";
     }
 
-    private String isPlangFactor(int number) {
+    public String isPlangFactor(int number) {
         return number != 0 && number % 5 == 0 ? "Plang" : "";
     }
 
-    private String isPlongFactor(int number) {
+    public String isPlongFactor(int number) {
         return number != 0 && number % 7 == 0 ? "Plong" : "";
     }
 }
--- src/test/java/com/sparta/as/RaindropsTests.java
+++ src/test/java/com/sparta/as/RaindropsTests.java
@@ -3,6 +3,7 @@ package com.sparta.as;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,7 +11,16 @@ public class RaindropsTests {
 
     private static Raindrops raindrops;
 
+    private static final String PLING = "Pling";
+    private static final String PLANG = "Plang";
+    private static final String PLONG = "Plong";
+    private static final String PLING_PLANG = "PlingPlang";
+    private static final String PLING_PLONG = "PlingPlong";
+    private static final String PLANG_PLONG = "PlangPlong";
+    private static final String PLING_PLANG_PLONG = "PlingPlangPlong";
+
     @BeforeAll
+
     static void setUpAll() {
         raindrops = new Raindrops();
     }
@@ -19,21 +29,21 @@ public class RaindropsTests {
     @ValueSource(ints = {3, -3})
     @DisplayName("Check that numbers with only a factor of 3 return 'Pling'")
     void checkThatNumbersWithOnlyAFactorOf3ReturnPling(int number) {
-        Assertions.assertEquals("Pling", raindrops.plingPlangPlong(number));
+        Assertions.assertEquals(PLING, raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5, -25})
     @DisplayName("Check that numbers with only a factor of 5 return 'Plang'")
     void checkThatNumbersWithOnlyAFactorOf5ReturnPlang(int number) {
-        Assertions.assertEquals("Plang", raindrops.plingPlangPlong(number));
+        Assertions.assertEquals(PLANG, raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {7, -14, 28})
     @DisplayName("Check that numbers with only a factor of 7 return 'Plong'")
     void checkThatNumbersWithOnlyAFactorOf7ReturnPlong(int number) {
-        Assertions.assertEquals("Plong", raindrops.plingPlangPlong(number));
+        Assertions.assertEquals(PLONG, raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
@@ -47,27 +57,39 @@ public class RaindropsTests {
     @ValueSource(ints = {15, -30, 30})
     @DisplayName("Check that numbers with a factor of 3 and 5 return 'PlingPlang'")
     void checkThatNumbersWithAFactorOf3And5ReturnPlingPlang(int number) {
-        Assertions.assertEquals("PlingPlang", raindrops.plingPlangPlong(number));
+        Assertions.assertEquals(PLING_PLANG, raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {21, -42})
     @DisplayName("Check that numbers with a factor of 3 and 7 return 'PlingPlong'")
     void checkThatNumbersWithAFactorOf3And7ReturnPlingPlong(int number) {
-        Assertions.assertEquals("PlingPlong", raindrops.plingPlangPlong(number));
+        Assertions.assertEquals(PLING_PLONG, raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {35, -70})
     @DisplayName("Check that numbers with a factor of 5 and 7 return 'PlangPlong'")
     void checkThatNumbersWithAFactorOf5And7ReturnPlangPlong(int number) {
-        Assertions.assertEquals("PlangPlong", raindrops.plingPlangPlong(number));
+        Assertions.assertEquals(PLANG_PLONG, raindrops.plingPlangPlong(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {105, -210})
     @DisplayName("Check that numbers with a factor of 3, 5 and 7 return 'PlingPlangPlong'")
     void checkThatNumbersWithAFactorOf35And7ReturnPlingPlangPlong(int number) {
-        Assertions.assertEquals("PlingPlangPlong", raindrops.plingPlangPlong(number));
+        Assertions.assertEquals(PLING_PLANG_PLONG, raindrops.plingPlangPlong(number));
+    }
+
+    @Nested
+    @DisplayName("Is 'Pling', 'Plang' or 'Plong' methods tests")
+    class isPlingPlangPlongMethodTests {
+
+        @ParameterizedTest
+        @ValueSource(ints = {3, -3})
+        @DisplayName("check isPlingFactor returns 'Pling'")
+        void checkIsPlingFactorReturnsPling(int number) {
+            Assertions.assertEquals(PLING, raindrops.isPlingFactor(number));
+        }
     }
 }
