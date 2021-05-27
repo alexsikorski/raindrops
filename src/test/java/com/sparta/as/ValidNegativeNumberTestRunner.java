26fa6a10c4b934104b98a4046ba1db5d62f8adbb
Alex Sikorski <alex@alexsikorski.net>
2021-05-27 19:03:19 +0100

    added test runner for invalid numbers and valid negative number inputs

--- src/test/java/com/sparta/as/ValidNegativeNumberTestRunner.java
+++ src/test/java/com/sparta/as/ValidNegativeNumberTestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources",
         plugin = {"pretty", "html:target/plingplangplong_report.html"},
-        tags = "@InvalidNumberTests"
+        tags = "@ValidNegativeNumberTests"
 )
-public class InvalidNumberTestRunner {
+public class ValidNegativeNumberTestRunner {
 }
